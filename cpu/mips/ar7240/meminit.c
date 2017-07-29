3495e05b4c9d4cd477543c1c7b30cf92aafa0416
Oleksij Rempel <linux@rempel-privat.de>
2017-07-29 12:24:46 +0200

    m cpu/mips/ar7240/meminit.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- cpu/mips/ar7240/meminit.c
+++ cpu/mips/ar7240/meminit.c
@@ -95,120 +95,18 @@ ar7240_ddr_find_size(void)
     return (i*AR7240_DDR_SIZE_INCR);
 }
 
-#ifndef CONFIG_WASP
-void
-ar7240_ddr_initial_config(uint32_t refresh)
-{
-	int ddr2 = 0,ddr_config;
-	int ddr_config2,ext_mod,ddr2_ext_mod;
-	int mod_val,mod_val_init;
-
-	prmsg("\nsri\n");
-#if 0
-	ar7240_reg_wr(AR7240_RESET, AR7240_RESET_DDR);
-	udelay(10);
-#endif
-	ddr2 = ((ar7240_reg_rd(0xb8050020) & 0x1) == 0);
-#ifdef ENABLE_DYNAMIC_CONF
-	if(*(volatile int *)CFG_DDR_MAGIC_F == CFG_DDR_MAGIC){
-		ddr_config = CFG_DDR_CONFIG_VAL_F;
-		ddr_config2 = CFG_DDR_CONFIG2_VAL_F;
-		ext_mod = CFG_DDR_EXT_MODE_VAL_F;
-		ddr2_ext_mod = ext_mod;
-	}
-	else
-#endif
-	{
-#ifdef CONFIG_SUPPORT_AR7241
-		if (is_ar7241() || is_ar7242()) {
-			if (ddr2) {
-				prmsg("%s(%d): virian ddr2 init\n", __func__, __LINE__);
-				ddr_config	= CFG_7241_DDR2_CONFIG_VAL;
-				ddr_config2	= CFG_7241_DDR2_CONFIG2_VAL;
-				ext_mod		= CFG_7241_DDR2_EXT_MODE_VAL;
-				ddr2_ext_mod	= CFG_DDR2_EXT_MODE_VAL;
-				mod_val_init	= CFG_7241_DDR2_MODE_VAL_INIT;
-				mod_val		= CFG_7241_DDR2_MODE_VAL;
-			} else {
-				prmsg("%s(%d): virian ddr1 init\n", __func__, __LINE__);
-				ddr_config	= CFG_7241_DDR1_CONFIG_VAL;
-				ddr_config2	= CFG_7241_DDR1_CONFIG2_VAL;
-				ext_mod		= CFG_7241_DDR1_EXT_MODE_VAL;
-				ddr2_ext_mod	= CFG_DDR2_EXT_MODE_VAL;
-				mod_val_init	= CFG_7241_DDR1_MODE_VAL_INIT;
-				mod_val		= CFG_7241_DDR1_MODE_VAL;
-			}
-		}
-		else
-#endif
-		{
-			prmsg("%s(%d): python ddr init\n", __func__, __LINE__);
-			ddr_config = CFG_DDR_CONFIG_VAL;
-			ddr_config2 = CFG_DDR_CONFIG2_VAL;
-			ext_mod = CFG_DDR_EXT_MODE_VAL;
-#ifndef CONFIG_WASP_EMU
-			ddr2_ext_mod = CFG_DDR2_EXT_MODE_VAL;
-#endif
-			mod_val_init = CFG_DDR_MODE_VAL_INIT;
-			mod_val = CFG_DDR_MODE_VAL;
-		}
-	}
-
-	if (ddr2) {
-		ar7240_reg_wr_nf(0xb800008c, 0xA59);
-		udelay(100);
-		ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x10);
-		udelay(10);
-		ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x20);
-		udelay(10);
-	}
-
-	ar7240_reg_wr_nf(AR7240_DDR_CONFIG, ddr_config);
-	udelay(100);
-	ar7240_reg_wr_nf(AR7240_DDR_CONFIG2, ddr_config2 | 0x80);
-	udelay(100);
-	ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x8);
-	udelay(10);
-
-	ar7240_reg_wr_nf(AR7240_DDR_MODE, mod_val_init);
-	udelay(1000);
-
-	ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x1);
-	udelay(10);
-
-	if (ddr2) {
-		ar7240_reg_wr_nf(AR7240_DDR_EXT_MODE, ddr2_ext_mod);
-	} else {
-		ar7240_reg_wr_nf(AR7240_DDR_EXT_MODE, ext_mod);
-	}
-
-	udelay(100);
-	ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x2);
-	udelay(10);
-	ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x8);
-	udelay(10);
-	ar7240_reg_wr_nf(AR7240_DDR_MODE, mod_val);
-	udelay(100);
-	ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x1);
-	udelay(10);
-	ar7240_reg_wr_nf(AR7240_DDR_REFRESH, refresh);
-	udelay(100);
-	ar7240_reg_wr_nf(AR7240_DDR_RD_DATA_THIS_CYCLE,
-				CFG_DDR_RD_DATA_THIS_CYCLE_VAL);
-	udelay(100);
-}
-#else
 int /* ram type */
 wasp_ddr_initial_config(uint32_t refresh)
 {
-#if !defined(CONFIG_ATH_NAND_BR)
 	int		ddr_config, ddr_config2, ext_mod, mod_val,
 			mod_val_init, cycle_val, tap_val, type;
+	// PLL_CONFIG_VAL_F		(PLL_FLASH_ADDR + CFG_FLASH_SECTOR_SIZE - 0x20)
 	uint32_t	*pll = (unsigned *)PLL_CONFIG_VAL_F;
 
 	prmsg("\nsri\n");
 	prmsg("Wasp 1.%d\n", ar7240_reg_rd(AR7240_REV_ID) & 0xf);
 
+	// 0xb80600b0, get 0x00bf057e (a) & 0x3 = 0x2 on tplink wdr4300
 	switch(WASP_RAM_TYPE(ar7240_reg_rd(WASP_BOOTSTRAP_REG))) {
 	case 0:
 	case 1:	// SDRAM
@@ -221,16 +119,22 @@ wasp_ddr_initial_config(uint32_t refresh)
 		break;
 		*/
 		prmsg("%s(%d): Wasp sdram\n", __func__, __LINE__);
+		// 0x7fbe8cd0
 		ddr_config	= CFG_934X_SDRAM_CONFIG_VAL;
+		// 0x959f66a8
 		ddr_config2	= CFG_934X_SDRAM_CONFIG2_VAL;
+		// 0x133
 		mod_val_init	= CFG_934X_SDRAM_MODE_VAL_INIT;
+		// 0x33
 		mod_val		= CFG_934X_SDRAM_MODE_VAL;
 		cycle_val	= CFG_SDRAM_RD_DATA_THIS_CYCLE_VAL;
 		tap_val		= CFG_934X_SDRAM_TAP_VAL;
 
+		// 0x18000108 0x13b
 		ar7240_reg_wr_nf(AR7240_DDR_CTL_CONFIG, 0x13b);
 		udelay(100);
 
+		// 0x18000118 0x3000001f
 		ar7240_reg_wr_nf(AR7240_DDR_DEBUG_RD_CNTL, 0x3000001f);
 		udelay(100);
 
@@ -238,35 +142,51 @@ wasp_ddr_initial_config(uint32_t refresh)
 
 		break;
 	case 2: // ddr2
+		// 0xc7d48cd0
 		ddr_config	= CFG_934X_DDR2_CONFIG_VAL;
+		// 0x9dd0e6a8
 		ddr_config2	= CFG_934X_DDR2_CONFIG2_VAL;
+		// 0x402
 		ext_mod		= CFG_934X_DDR2_EXT_MODE_VAL;
+		// 0x133
 		mod_val_init	= CFG_934X_DDR2_MODE_VAL_INIT;
+		// 0x33
 		mod_val		= CFG_934X_DDR2_MODE_VAL;
-		cycle_val	= CFG_DDR2_RD_DATA_THIS_CYCLE_VAL;
+		// 0x10012
 		tap_val		= CFG_934X_DDR2_TAP_VAL;
 
+		// 0x180000b8 0x1659
 		ar7240_reg_wr_nf(AR7240_DDR_DDR2_CONFIG, CFG_934X_DDR2_EN_TWL_VAL);
 		udelay(100);
+		// 0x18000010 0x10
 		ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x10);
 		udelay(10);
+		// 0x18000010 0x20
 		ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x20);
 		udelay(10);
 		prmsg("%s(%d): (", __func__, __LINE__);
+		// 0xb8060090, get 0x00002122
 		if (ar7240_reg_rd(AR7240_REV_ID) & 0xf) {
 							/* NAND Clear */
+			// 0xb80600b0, get 0x00bf057e 
 			if (ar7240_reg_rd(WASP_BOOTSTRAP_REG) & (1 << 3)) {
+				// executet on wdr4300
 				prmsg("32");
+				// 0x18000108 0x40
 				ar7240_reg_wr_nf(AR7240_DDR_CTL_CONFIG, (1 << 6));
+				// 0xff
 				cycle_val = CFG_DDR2_RD_DATA_THIS_CYCLE_VAL_32;
 			} else {
 				prmsg("16");
+				// 0x18000108 0x40
 				ar7240_reg_rmw_set(AR7240_DDR_CTL_CONFIG, (1 << 6));
+				// 0xffff
 				cycle_val = CFG_DDR2_RD_DATA_THIS_CYCLE_VAL_16;
 			}
 		} else {
 #if DDR2_32BIT_SUPPORT
 			prmsg("32");
+			// 0x18000108 0
 			ar7240_reg_wr_nf(AR7240_DDR_CTL_CONFIG, 0);
 #else
 			prmsg("16");
@@ -279,44 +199,25 @@ wasp_ddr_initial_config(uint32_t refresh)
 		break;
 	case 3: // ddr1
 		prmsg("%s(%d): Wasp (16bit) ddr1 init\n", __func__, __LINE__);
+		// 0x7fd48cd0
 		ddr_config	= CFG_934X_DDR1_CONFIG_VAL;
+		// 0x99d0e6a8
 		ddr_config2	= CFG_934X_DDR1_CONFIG2_VAL;
+		// 0x2
 		ext_mod		= CFG_934X_DDR1_EXT_MODE_VAL;
+		// 0x133
 		mod_val_init	= CFG_934X_DDR1_MODE_VAL_INIT;
+		// 0x33
 		mod_val		= CFG_934X_DDR1_MODE_VAL;
+		// 0xffff
 		cycle_val	= CFG_DDR1_RD_DATA_THIS_CYCLE_VAL;
+		// 0x14
 		tap_val		= CFG_934X_DDR1_TAP_VAL;
 		type = 2;
 		break;
 	}
-#if 0
-	if (*pll == PLL_MAGIC) {
-		uint32_t cas = pll[5];
-		if (cas == 3 || cas == 4) {
-			cas = (cas * 2) + 1;
-			ddr_config &= ~(DDR_CONFIG_CAS_LATENCY_MSB_MASK |
-					DDR_CONFIG_CAS_LATENCY_MASK);
-			ddr_config |= DDR_CONFIG_CAS_LATENCY_SET(cas & 0x7) |
-				DDR_CONFIG_CAS_LATENCY_MSB_SET((cas >> 3) & 1);
 
-			cas = pll[5];
-
-			ddr_config2 &= ~DDR_CONFIG2_GATE_OPEN_LATENCY_MASK;
-			ddr_config2 |= DDR_CONFIG2_GATE_OPEN_LATENCY_SET(2 * cas);
-
-			if (type == 1) {
-				uint32_t tmp;
-				tmp = ar7240_reg_rd(AR7240_DDR_DDR2_CONFIG);
-				tmp &= ~DDR2_CONFIG_DDR2_TWL_MASK;
-				tmp |= DDR2_CONFIG_DDR2_TWL_SET(cas == 3 ? 3 : 5);
-				ar7240_reg_wr_nf(AR7240_DDR_DDR2_CONFIG, tmp);
-			}
-
-			mod_val = (cas == 3 ? 0x33 : 0x43);
-			mod_val_init = 0x100 | mod_val;
-		}
-	}
-#else
+	// 0x9f04ffe0 0xaabbccdd
 	if (*pll == PLL_MAGIC) {
 		uint32_t cas = pll[5];
 		if (cas == 3 || cas == 4) {
@@ -343,49 +244,69 @@ wasp_ddr_initial_config(uint32_t refresh)
 			mod_val_init = 0x100 | mod_val;
 		}
 	}
-#endif
 
+	// 0x18000000
 	ar7240_reg_wr_nf(AR7240_DDR_CONFIG, ddr_config);
 	udelay(100);
+	// 0x18000004 ddr_config2 | 0x80
 	ar7240_reg_wr_nf(AR7240_DDR_CONFIG2, ddr_config2 | 0x80);
 	udelay(100);
+	// 0x18000010 0x8
 	ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x8);
 	udelay(10);
 
+	// 0x18000008
 	ar7240_reg_wr_nf(AR7240_DDR_MODE, mod_val_init);
 	udelay(1000);
 
+	// 0x18000010 0x1
 	ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x1);
 	udelay(10);
 
+	// true for ddr2
 	if (type == 1) {
+		// 0x1800000c 0x382
 		ar7240_reg_wr_nf(AR7240_DDR_EXT_MODE, CFG_934X_DDR2_EXT_MODE_VAL_INIT);
 		udelay(100);
+		// 0x18000010 0x2
 		ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x2);
 		udelay(10);
 	}
+
+	// true for ddr2
 	if (type != 0) {
+		// 0x1800000c
 		ar7240_reg_wr_nf(AR7240_DDR_EXT_MODE, ext_mod);
 	}
 
 	udelay(100);
+	// 0x18000010 0x2
 	ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x2);
 	udelay(10);
+	// 0x18000010 0x8
 	ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x8);
 	udelay(10);
+	// 0x18000008
 	ar7240_reg_wr_nf(AR7240_DDR_MODE, mod_val);
 	udelay(100);
+	// 0x18000010
 	ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x1);
 	udelay(10);
+	// 0x18000014
 	ar7240_reg_wr_nf(AR7240_DDR_REFRESH, refresh);
 	udelay(100);
 
-        ar7240_reg_wr (AR7240_DDR_TAP_CONTROL0, tap_val);
+	// 0x1800001c
+    ar7240_reg_wr (AR7240_DDR_TAP_CONTROL0, tap_val);
+	// 0x18000020
 	ar7240_reg_wr (AR7240_DDR_TAP_CONTROL1, tap_val);
 
+	// 0xb8060090
 	if (ar7240_reg_rd(AR7240_REV_ID) & 0xf) {
 						/* NAND Clear */
+		// 0xb80600b0, get 0x00bf057e & 0x8 = true && type = 1 
 		if ((ar7240_reg_rd(WASP_BOOTSTRAP_REG) & (1 << 3)) && type) {
+			// executet on wdr4300
 			ar7240_reg_wr (AR7240_DDR_TAP_CONTROL2, tap_val);
 			ar7240_reg_wr (AR7240_DDR_TAP_CONTROL3, tap_val);
 		}
@@ -398,142 +319,17 @@ wasp_ddr_initial_config(uint32_t refresh)
 #endif
 	}
 
+	// 0x18000018 cycle_val
 	ar7240_reg_wr_nf(AR7240_DDR_RD_DATA_THIS_CYCLE, cycle_val);
 	udelay(100);
+	// 0x180000c4 0x74444444
 	ar7240_reg_wr_nf(AR7240_DDR_BURST, 0x74444444);
 	udelay(100);
+	// 0x180000c8 0x222
 	ar7240_reg_wr_nf(AR7240_DDR_BURST2, 0x222);
 	udelay(100);
+	// 0x180000cc
 	ar7240_reg_wr_nf(AR7240_AHB_MASTER_TIMEOUT, 0xfffff);
 	udelay(100);
-#if (CFG_PLL_FREQ == CFG_PLL_600_500_250) || \
-    (CFG_PLL_FREQ == CFG_PLL_500_500_250)
-	// PMU2 ddr ldo tune
-	ar7240_reg_rmw_set(0xb8116c44, (0x3 << 19));
-	udelay(100);
-#endif
 	return type;
-#else
-	return 0;
-#endif
 }
-#endif
-
-#ifdef CONFIG_HORNET_EMU
-void
-ar7240_ddr_initial_config_for_fpga(void)
-{
- #ifdef CONFIG_HORNET_EMU_HARDI
-    /* HARDI FPGA board */
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x4);          /* Auto-Refresh */
-    udelay(10);      
-    ar7240_reg_wr_nf(AR7240_DDR_REFRESH, 0x4100);
-    udelay(10);  
-        
-    ar7240_reg_wr_nf(AR7240_DDR_CONFIG, 0x7fd68cd0);    /* Cas Latency 7 */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_CONFIG2, 0x959ec6a8);   /* Gate Open 5 */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_DDR2_CONFIG, 0x858);    /* Disable DDR2, set Write Latency */
-    udelay(10);    
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x8);          /* precharge all */
-    udelay(10);       
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x8);          /* precharge all */
-    udelay(10);       
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x1);          /* MR update */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_MODE, 0x133);           /* Mode Word Settings  */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_EXT_MODE, 0x0);         /* Extended Mode Word Settings */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_RD_DATA_THIS_CYCLE, 0xff);  /* DDR read data capture bit mask */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x1);          /* MR update */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x2);          /* EMR update */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x8);          /* precharge all */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x8);          /* precharge all */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x4);          /* Auto-Refresh */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x4);          /* Auto-Refresh */
-    udelay(10);        
-    ar7240_reg_wr_nf(AR7240_DDR_MODE, 0x133);           /* Mode Word Settings  */
-    udelay(10);    
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x1);          /* MR update */    
-    udelay(10); 
-    ar7240_reg_wr_nf(AR7240_DDR_EXT_MODE, 0x382);       /* Extended Mode Word Settings */     
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x2);          /* EMR update */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_EXT_MODE, 0x402);       /* Extended Mode Word Settings */     
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x2);          /* EMR update */
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_MODE, 0x33);            /* Mode Word Settings  */
-    udelay(10);    
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x1);          /* MR update */    
-    udelay(10);
-    #ifdef CONFIG_HORNET_EMU_HARDI_WLAN
-    /* 
-     * Emulation board reference clock is 48 MHz(20.833ns)
-     * DDR request 64ms has 8192 refresh
-     * So each refresh interval is 64ms/8192 = 7812.5 ns
-     * i.e ddr have to refresh in 7812.5/20.833 = 375 = 0x177 clock cycles     
-     */
-    ar7240_reg_wr_nf(AR7240_DDR_REFRESH, 0x4177);
-    #else
-    /* 
-     * Emulation board reference clock is 80 MHz(12.5ns)
-     * DDR request 64ms has 8192 refresh
-     * So each refresh interval is 64ms/8192 = 7812.5 ns
-     * i.e ddr have to refresh in 7812.5/12.5 = 625 = 0x271 clock cycles
-     */
-    ar7240_reg_wr_nf(AR7240_DDR_REFRESH, 0x4271);
-    #endif
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_TAP_CONTROL0, 0x0);
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_TAP_CONTROL1, 0x0);
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_TAP_CONTROL2, 0x0);
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_TAP_CONTROL3, 0x0);
-    udelay(10);
- #else
-    /* EBU FPGA board */
-    ar7240_reg_wr_nf(AR7240_DDR_CONFIG, 0xc7bc8cd0); /* Cas Latency 8 */
-    udelay(100);
-    ar7240_reg_wr_nf(AR7240_DDR_CONFIG2, 0x95d0e6a8); /* Gate Open 5 */
-    udelay(100);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x8);
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_MODE, 0x123);
-    udelay(100);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x1);
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_EXT_MODE, 0x1);
-    udelay(100);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x2);
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x8);
-    udelay(10);
-    ar7240_reg_wr_nf(AR7240_DDR_MODE, 0x23);
-    udelay(100);
-    ar7240_reg_wr_nf(AR7240_DDR_CONTROL, 0x1);
-    udelay(10);
-    /* 
-     * Emulation board reference clock is 80 MHz(12.5ns)
-     * DDR request 64ms has 8192 refresh
-     * So each refresh interval is 64ms/8192 = 7812.5 ns
-     * i.e ddr have to refresh in 7812.5/12.5 = 625 = 0x271 clock cycles     
-     */
-    ar7240_reg_wr_nf(AR7240_DDR_REFRESH, 0x4271);
-    udelay(100);
-    ar7240_reg_wr_nf(AR7240_DDR_RD_DATA_THIS_CYCLE, 0x00ff);
-    udelay(100);
- #endif
-}
-#endif
